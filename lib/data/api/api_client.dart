09345a3586ae242bb201707bb075fcbb9d47d208
Coal_Yu <53548391+SuperGripG4F@users.noreply.github.com>
2023-01-31 00:13:34 +0800

    created a simple network request class (not complete)

---
+++ lib/data/api/api_client.dart
@@ -0,0 +1,31 @@
+import 'package:get/get_connect.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class ApiClient extends GetConnect implements GetxService {
+  late String token;
+  //server url
+  final String appBaseUrl;
+
+  //storing data
+  late Map<String, String> _mainHeaders;
+
+  ApiClient({required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    //trying lifetime
+    timeout = Duration(seconds: 30);
+    _mainHeaders = {
+      'Content-type': 'application/json; charset=UTF-8',
+      //Bearer is used for Authorization
+      'Authorization': 'Bearer $token',
+    };
+  }
+
+  Future<Response> getData(String uri) async {
+    try {
+      Response response = await get(uri);
+      return response;
+    } catch (e) {
+      return Response(statusCode: 1, statusText: e.toString());
+    }
+  }
+}
